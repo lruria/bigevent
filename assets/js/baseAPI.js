12f999906a89dcf7d10697ead1eb5fa64d0020b3
lruri <2293453986@qq.com>
2021-01-30 15:22:27 +0800

    完成首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
-$.ajaxPrefilter(function(options){
-    // 在发起ajax请求之前统一拼接请求的根路径
-      options.url = 'http://ajax.frontend.itheima.net'+options.url
+$.ajaxPrefilter(function (options) {
+  // 在发起ajax请求之前统一拼接请求的根路径
+  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  options.complete =function(res){
+    if(res.responseJSON.status ===1 && res.responseJSON.message === '身份认证失败！'){
+      localStorage.removeItem('token')
+      location.href = '/code/login.html'
+    }
+  }
+
 })
